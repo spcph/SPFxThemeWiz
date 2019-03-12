c1ac9e1a1f4504606b3e7148bd8a40313f0717f2
Morten Nordholt Andersen <man@peoplenet.dk>
2019-03-12 10:57:03 +0100

    Updated UI

--- src/webparts/themeWiz/components/ThemeWiz.tsx
+++ src/webparts/themeWiz/components/ThemeWiz.tsx
@@ -4,14 +4,17 @@ import reactCSS from 'reactcss';
 import { SketchPicker } from 'react-color';
 import { loadTheme, getTheme, ITheme, IPalette } from 'office-ui-fabric-react';
 import { IPartialTheme } from '@uifabric/styling';
-import { DefaultButton, Panel, PanelType, Slider } from 'office-ui-fabric-react';
+import { DefaultButton, Panel, PanelType, Slider, Checkbox } from 'office-ui-fabric-react';
 import * as $ from 'jquery';
+import { TinyColor } from '@ctrl/tinycolor';
 
 export interface IThemeWizState {
   originalPalette: IPalette;
   newPalette: IPalette;
   activePicker: string;
-  isPanelOpen:boolean;
+  isPanelOpen: boolean;
+  shouldSpreadColor: boolean;
+  spreadFactor: number;
 }
 
 export default class ThemeWiz extends React.Component<{}, IThemeWizState> {
@@ -24,7 +27,9 @@ export default class ThemeWiz extends React.Component<{}, IThemeWizState> {
       originalPalette: null,
       newPalette: null,
       activePicker: '',
-      isPanelOpen: false
+      isPanelOpen: false,
+      shouldSpreadColor: false,
+      spreadFactor: 10
     };
   }
 
@@ -57,7 +62,8 @@ export default class ThemeWiz extends React.Component<{}, IThemeWizState> {
         popover: {
           position: 'absolute',
           zIndex: '2',
-          left: '-125px'
+          left: '-215px',
+          top: '-1px'
         },
         cover: {
           position: 'fixed',
@@ -116,33 +122,34 @@ export default class ThemeWiz extends React.Component<{}, IThemeWizState> {
     json = 'data:text/plain;base64,' + btoa(json);
     //Format the PowerShell file
     let ps:string = JSON.stringify(this.state.newPalette);
+    ps = ps.replace(/}/g, ';}',);
     ps = ps.replace(/:/g, ' = ',);
     ps = ps.replace(/,"/g, ';\n\t"');
     ps = ps.replace(/{/g, '{\n\t');
     ps = ps.replace(/}/g, '\n}');
-    ps = '@' + ps;
+    ps = '$theme = @' + ps;
     ps = 'data:text/plain;base64,' + btoa(ps);
 
     return (
       <div>
+        <Checkbox label='Spread theme colors from primary' onChange={(ev: React.FormEvent<HTMLElement>, isChecked: boolean): void => { this.setState({shouldSpreadColor: isChecked });}} />
+        <br/>
         <Slider
-            label="Make panel transparent to show underlying content"
+            label="Spread amount"
             min={0}
-            max={1}
-            step={0.1}
-            defaultValue={1}
+            max={33}
+            step={1}
+            defaultValue={10}
             showValue={false}
-            componentRef={(slider) => this.slider = slider}
-            // tslint:disable-next-line:jsx-no-lambda
+            disabled={!this.state.shouldSpreadColor}
             onChange={(value: any) => {
-              let slider:JQuery<Element> = $(document.activeElement);
-              slider.closest('.ms-Panel-main').css('background-color', 'rgb(255,255,255,' + value + ')');
+              this.setState({spreadFactor: value});
             }}
           />
           <br/>
-          <a href={json} download={'Theme.json'}>Download JSON file</a>
+          <a href={json} download={'Theme.json'}>JSON</a>
           <br/>
-          <a href={ps} download={'Theme.ps1'}>Download PowerShell file</a>
+          <a href={ps} download={'Theme.ps1'}>PowerShell</a>
           <br/>
           <br/>
         <DefaultButton onClick={this.onClosePanel}>Dismiss</DefaultButton>
@@ -171,6 +178,14 @@ export default class ThemeWiz extends React.Component<{}, IThemeWizState> {
   private handleChange = (color) => {
     let palette: IPalette = this.state.newPalette;
     palette[this.state.activePicker] = color.hex;
+    if(this.state.shouldSpreadColor && this.state.activePicker == 'themePrimary') {
+      palette['themeDarkAlt'] = new TinyColor(color.hex).shade(this.state.spreadFactor).toHexString();
+      palette['themeDark'] = new TinyColor(color.hex).shade(this.state.spreadFactor * 1.5).toHexString();
+      palette['themeDarker'] = new TinyColor(color.hex).shade(this.state.spreadFactor * 2).toHexString();
+      palette['themeLight'] = new TinyColor(color.hex).tint(this.state.spreadFactor).toHexString();
+      palette['themeLighter'] = new TinyColor(color.hex).tint(this.state.spreadFactor * 2).toHexString();
+      palette['themeLighterAlt'] = new TinyColor(color.hex).tint(this.state.spreadFactor * 3).toHexString();
+    }
     this.setState({ newPalette: palette });
   }
 
